9a590e1537af16883c136f801ac33120ea9eae99
Bastiaan Evers <evers.bastiaan@gmail.com>
2014-10-15 14:23:35 +0200

    Add off-target analysis

--- FindSites/QualityScoreSites.pl
+++ FindSites/QualityScoreSites.pl
@@ -2,18 +2,18 @@ use Getopt::Std;
 use warnings;
 use strict;
 
-sub IsPositionNearExon($$);
+sub IsPositionInGene($$);
 
 my $ScriptName="QualityScoreSites.pl";
 my %opts;
 my %RelativesFound;
 my %ExonicRelativesFound;
 my %OutputText;
-our %Exons;
+our %Genes;
 my $ProcessRelatives = 0;
 my $AdditionalIdenticalTargetsFound;
 my $Margin = 100;
-my $RefSeqFile = '/home/NKI/b.evers/RefSeq/refGene.txt';
+my $RefSeqFile = '../refseq/hg19.txt';
 
 getopt( 'oids', \%opts );
 die "ERROR in $ScriptName: No Outputfile given.\n" unless my $OutputFile = $opts{'o'};
@@ -24,19 +24,16 @@ getopts('l',\%opts);
 my $SkipLookingAtLast = $opts{'l'};
 
 #Load in all exonic position and make a hash off that, extending the exons with a certain margin
-open (IN, $RefSeqFile) or die "ERROR in $ScriptName: Cannot open refseqfile $RefSeqFile\n";
+open (IN, $RefSeqFile) or die "ERROR in $0: Cannot open refseqfile $RefSeqFile\n";
 while (defined(my $Line = <IN>)) {
 	chomp($Line);
 	my @RefSeqValues = split( /\t/, $Line );
 	my $Chromosome = substr( $RefSeqValues[2], 3 );
-	my $NumberOfExons  = $RefSeqValues[8];
-	my @ExonStartSites = split( /,/, $RefSeqValues[9] );
-	my @ExonEndSites   = split( /,/, $RefSeqValues[10] );
-	for (my $i=0;$i<@ExonStartSites;$i++) {
-		push (@{$Exons{$Chromosome}}, [$ExonStartSites[$i] - $Margin, $ExonEndSites[$i] + $Margin]); 
-	}
+	my $GeneStart = $RefSeqValues[4];
+	my $GeneEnd = $RefSeqValues[5];
+	push (@{$Genes{$Chromosome}}, [$GeneStart, $GeneEnd]); 
 }
-close (IN) or die "ERROR in $ScriptName: Cannot close refseqfile $RefSeqFile\n";
+close (IN) or die "ERROR in $0: Cannot close refseqfile $RefSeqFile\n";
 
 #Create a file with all relatives of the unique target sites with a certain distance
 #Read in the sam file, skip records with additional identical targets, skip records for which relatives were found in the previous run, unless l flag is set 
@@ -47,9 +44,8 @@ while (defined(my $Line=<IN>)) {
 	chomp($Line);
 	my @SAMValues = split( /\t/, $Line );
 	my $NumberOfColumns = (scalar @SAMValues);
-	
-	my $ProtospacerSequence = $SAMValues[9];
-	if($SAMValues[1] & 16) {
+	my $ProtospacerSequence = $SAMValues[10];
+	if($SAMValues[2] & 16) {
 		$ProtospacerSequence =~ tr/ACTG/TGAC/;
 		$ProtospacerSequence = reverse($ProtospacerSequence);
 	}
@@ -58,7 +54,7 @@ while (defined(my $Line=<IN>)) {
 	
 	$ExonicRelativesFound{$TargetSequence}=0;
 	my $FoundInPreviousRun = 0;
-	if ($NumberOfColumns>=20) {
+	if (!$SkipLookingAtLast) {
 		$FoundInPreviousRun = $SAMValues[$NumberOfColumns - 2];
 	}
 	$RelativesFound{$TargetSequence} = -1;
@@ -74,7 +70,7 @@ close (IN) or die "ERROR in $ScriptName: Cannot close inputfile $InputFile\n";
 
 #Map all the relatives to the genome. Write everything that maps to a separate file
 if ($ProcessRelatives) {
-	`/media/Data/iKRUNC/bowtie2-2.1.0/bowtie2 /media/Data/iKRUNC/hg19-index/hg19 -f $PotentialRelativesFile -t --no-hd --score-min L,-5,0 -a --mm -S $RelativesMatchFile`;	
+	`/media/Data/iKRUNC/bowtie2-2.1.0/bowtie2 /media/Data/iKRUNC/hg19-index/hg19 -f $PotentialRelativesFile -t --no-hd --score-min L,-5,0 -a -S $RelativesMatchFile`;	
 	
 	#Read in everything that was mapped
 	open (IN, $RelativesMatchFile) or die "ERROR in $ScriptName: Cannot open relatives match file $RelativesMatchFile\n";
@@ -83,19 +79,21 @@ if ($ProcessRelatives) {
 		chomp($Line);
 		my @SAMValues = split( /\t/, $Line );
 		if(!($SAMValues[1] & 4)) {
-			print OUT $Line . "\n";
 			my $ProtospacerSequence = $SAMValues[0];
 			my $TargetSequence=substr($ProtospacerSequence,13,20);
 			$RelativesFound{$TargetSequence}++;
-			if (IsPositionNearExon(substr($SAMValues[2], 3),($SAMValues[3] + ($SAMValues[1] & 16 ? 6 : 17)))) {
+			my $IsInGene=0;
+			if (IsPositionInGene(substr($SAMValues[2], 3),($SAMValues[3] + ($SAMValues[1] & 16 ? 6 : 17)))) {
 				$ExonicRelativesFound{$TargetSequence}++;
+				$IsInGene=1;
 			}
+			print OUT $TargetSequence . "\t" . $Line . "\t" . $IsInGene . "\n";
 		}
 	}
 	close (IN) or die "ERROR in $ScriptName: Cannot close relatives match file $RelativesMatchFile\n";
 	close (OUT) or die "ERROR in $ScriptName: Cannot close outputfile " . ($OutputFile . ".relatives") . "\n";
-	unlink $RelativesMatchFile;
-	unlink $PotentialRelativesFile;
+#	unlink $RelativesMatchFile;
+#	unlink $PotentialRelativesFile;
 }
 
 #Write the output file, with an extra column indicating how many relatives were found and one indicating how many of those were found near exons
@@ -107,16 +105,17 @@ foreach my $TargetSequence (sort {$a cmp $b} keys %OutputText) {
 }	
 close (OUT) or die "ERROR in $ScriptName: Cannot close outputfile $OutputFile\n";
 
-sub IsPositionNearExon($$) {
+sub IsPositionInGene($$) {
 	my($Chromosome, $Position) = @_;
-	my $PositionIsNearExon = 0;
-	foreach my $Exon (@{$Exons{$Chromosome}}) {
-		my $StartSite = ${$Exon}[0];
-		my $EndSite = ${$Exon}[1];
+	my $IsPositionInGene = 0;
+	foreach my $Gene (@{$Genes{$Chromosome}}) {
+		my $StartSite = ${$Gene}[0];
+		my $EndSite = ${$Gene}[1];
+		
 		if ($Position > $StartSite && $Position <= $EndSite) {
-			$PositionIsNearExon = 1;
+			$IsPositionInGene = 1;
 			last;		
 		}
 	}
-	return $PositionIsNearExon;
+	return $IsPositionInGene;
 }
