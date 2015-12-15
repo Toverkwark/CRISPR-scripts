c6948302df86023f6da8be5670b3d840516d158a
Bastiaan Evers <evers.bastiaan@gmail.com>
2015-12-15 13:49:28 +0100

    Update QuickInsertAnalysis.pl

--- ScreenAnalysis/QuickInsertAnalysis.pl
+++ ScreenAnalysis/QuickInsertAnalysis.pl
@@ -1,5 +1,6 @@
 #!/bin/perl
 $Search="GGCTTTATATATCTTGTGGAAAGGACGAAACACCG";
+$Library="/home/NFS/users/b.evers/libraries/crispr.epigenetics.library.csv";
 my $File=open(IN,"small");
 my $Line=<IN>;
 my $Line=<IN>;
@@ -9,13 +10,28 @@ while (defined($Line=<IN>)) {
         $Line=<IN>;
         $Position=index($Line,"GGCTTTATATATCTTGTGGAAAGGACGAAACACCG");
         if($Position) {
-                $Sequence=substr($Line,$Position+36,20);
+                $Sequence=substr($Line,$Position+35,20);
                 $Sequences{$Sequence}++;
         }
 
         $Line=<IN>;
         $Line=<IN>;
 }
+$Total=0;
+$InLibraryTotal=0;
 foreach $Sequence (sort {$Sequences{$a} <=> $Sequences{$b}} keys %Sequences) {
-        print "$Sequence:\t" . $Sequences{$Sequence} . "\n";
+        $InLibrary=`grep -P "$Sequence" $Library`;
+        print "$Sequence:\t" . $Sequences{$Sequence} . "\t";
+        if ($InLibrary) {
+                print "yes\n";
+                $InLibraryTotal=$InLibraryTotal+$Sequences{$Sequence};
+        }
+        else {
+                print "no\n";
+        }
+
+
+        $Total=$Total+$Sequences{$Sequence};
 }
+print "In Library:$InLibraryTotal\n";
+print "Total:$Total\n";
