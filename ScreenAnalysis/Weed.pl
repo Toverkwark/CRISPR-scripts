70b0481923508f68e211f273b566c6f4ce20cce2
Bastiaan Evers <evers.bastiaan@gmail.com>
2016-05-27 13:45:03 +0200

    Also remove CDKN11A

--- ScreenAnalysis/Weed.pl
+++ ScreenAnalysis/Weed.pl
@@ -4,7 +4,7 @@ open(OUT,'>',"weeded");
 while(defined($Line=<IN>)) {
 	chomp($Line);
 	my @Values=split(/\t/,$Line);	
-	if($Values[0] ne 'ERBB2-2' && $Values[0] ne 'FGR-1' && $Values[0] ne 'HCK-3' && $Values[0] ne 'MAP3K19-2' && $Values[0] ne 'PAK7-2' && $Values[0] ne 'PRKD2-2' && $Values[0] ne 'PRKD3-2' && $Values[0] ne 'STK35-1' && $Values[0] ne 'TSSK4-2' && $Values[0] ne 'TSSK4-4' && $Values[0] ne 'ULK2-3' && $Values[1] ne 'Vector' && $Values[3] ne 'Vector') {	
+	if($Values[0] ne 'ERBB2-2' && $Values[0] ne 'FGR-1' && $Values[0] ne 'HCK-3' && $Values[0] ne 'MAP3K19-2' && $Values[0] ne 'PAK7-2' && $Values[0] ne 'PRKD2-2' && $Values[0] ne 'PRKD3-2' && $Values[0] ne 'STK35-1' && $Values[0] ne 'TSSK4-2' && $Values[0] ne 'TSSK4-4' && $Values[0] ne 'ULK2-3' && $Values[1] ne 'Vector' && $Values[3] ne 'Vector' && $Values[1] ne 'CDK11A') {	
 		if(substr($Values[1],0,6) eq 'HGLibA') {
 			$Values[1]='NonTargetingControl'
 		}
